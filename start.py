d7eb3c6d5bd381c725ed1f6e96b5a3c13c42ebac
zcm1995 <2449315455@qq.com>
2022-08-11 16:29:08 +0800

    优化编码结构

--- start.py
+++ start.py
@@ -1,35 +1,37 @@
-
-#导包,发起请求使用urllib库的request请求模块
+# 导包,发起请求使用urllib库的request请求模块
 # from io import BytesIO
 # import urllib.request
 # import gzip
 import requests
 
+
 # 读取html内容
-def readhtml(url):
-    res = requests.get(url)
+def readhtml(txt_url):
+    res = requests.get(txt_url)
     txt = res.text
     txt = txt.encode('ISO-8859-1').decode('utf-8')
     # print(txt)
     return txt
 
+
 # 输出小说章节内容
-def splittxt(url):
-    txt = readhtml(url)
+def splittxt(txt_url):
+    txt = readhtml(txt_url)
     j = '&nbsp;&nbsp;&nbsp;&nbsp;'
     t = txt.split(j)
     # print(len(t))
     # print(t)
-    i = '<br/><br/>'
+    it = '<br/><br/>'
     for l in t:
         # print(l)
-        if i in l:
-            print(l.split(i)[0])
+        if it in l:
+            print(l.split(it)[0])
 
 
 # 获取固定章节地址
-def getchapternum(baseurl, n):
-    txt = readhtml(baseurl)
+def getchapternum(base_url, num):
+    global i
+    txt = readhtml(base_url)
     # print(txt)
     h = '<dd><a href="'
     chapternum = txt.split(h)
@@ -39,34 +41,36 @@ def getchapternum(baseurl, n):
         i = '.html'
         if i in chapternum[l]:
             chapternum[l] = chapternum[l].split(i)[0]
-    if n > 0 and n < len(chapternum):
-        return baseurl + chapternum[n] + i
+    if 0 < num < len(chapternum):
+        return base_url + chapternum[num] + i
     else:
         return False
 
+
 # 获取下一章网址
-def getnexturl(url):
-    url = readhtml(url)
+def get_next_url(txt_url):
+    txt_url = readhtml(txt_url)
     # print(txt)
     h = '章节列表'
     j = '">下一章'
     k = 'href="'
-    if h in url:
-        url = url.split(h)[1]
+    if h in txt_url:
+        txt_url = txt_url.split(h)[1]
     else:
         return False
     # print(url)
-    if j in url:
-        url = url.split(j)[0]
+    if j in txt_url:
+        txt_url = txt_url.split(j)[0]
     else:
         return False
     # print(url)
-    if k in url:
-        url = url.split(k)[1]
+    if k in txt_url:
+        txt_url = txt_url.split(k)[1]
     else:
         return False
     # print(url)
-    return url
+    return txt_url
+
 
 if __name__ == '__main__':
     # 书香小说网
@@ -77,23 +81,22 @@ if __name__ == '__main__':
     # url = 'https://www.shuquge.com/'
     # url = 'https://www.xbiquge.la/'
     baseurl = 'https://www.xbiquwx.la/'
-    txtnum = '112_112946'     # 万界竞技，开局我选张三丰
+    txtnum = '112_112946'  # 万界竞技，开局我选张三丰
     # txtnum = input('请输入小说代码:')
     baseurl = baseurl + txtnum + '/'
     # print(baseurl)
     # print(chapternum)
-    i = 1
-    n = 191
+    n = 190
     # n = input('请选择章节:')
     url = getchapternum(baseurl, int(n))
-    while i == 1:
+    while True:
         splittxt(url)
         n = input('请选择章节:')
         if n == '':
-            url = getnexturl(url)
+            url = get_next_url(url)
         else:
             url = getchapternum(baseurl, int(n))
-        if url == False:
+        if not url:
             print('输入章节有误')
             break
         if n == 'e':
